161110245954ad59f300c3e02f9edba3a1672d47
ahmetemrekilinc <aekilinc@gmail.com>
2023-12-27 00:22:17 +0300

    4.1.1 react-validatable-form is updated to 5.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    optimizeDeps: {
+        include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip'],
+    },
     plugins: [
         react({
             jsxImportSource: '@emotion/react',
